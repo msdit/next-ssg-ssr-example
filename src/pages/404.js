67a595fce40b4a4c4bb550964a91f5f206523dfe
Masoud IT <maghaie148@gmail.com>
2022-06-02 17:15:39 +0430

    404-document-script

---
+++ src/pages/404.js
@@ -0,0 +1,7 @@
+const E404 = () => {
+  return (
+    <div>404</div>
+  )
+}
+
+export default E404
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,18 @@
 import "../styles/globals.css";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   return (
     <div style={{ margin: 10, border: "2px solid purple" }}>
       <Component {...pageProps} />
+
+      <Script
+        id="imber"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `alert('hi')`,
+        }}
+      />
     </div>
   );
 }
